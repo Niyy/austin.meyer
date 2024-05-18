ac186ac16020bbce5dc23ac5e88758d8b0b5dacc
austin meyer <meyerforge@gmail.com>
2024-05-18 14:25:21 -0400

    daughter awoke. working on putting together this nav bar.

--- MySite/obj/Debug/net8.0/MySite.AssemblyInfo.cs
+++ MySite/obj/Debug/net8.0/MySite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1792153a31540c52e802d4501332507e6cda8098")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
